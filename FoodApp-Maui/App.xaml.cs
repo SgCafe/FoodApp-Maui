d8b654b827eb8bc6804f885918e06ffaf75a3d45
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2024-03-11 19:42:42 -0300

    Adicionar arquivos de projeto.

---
+++ FoodApp-Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FoodApp_Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
